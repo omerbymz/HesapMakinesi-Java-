077545fe622d61b100e6664f938c15e35b29c6ed
Omercan Baymaz <obaymaz36@gmail.com>
2022-02-27 21:57:59 +0300

    Switch-case hesap makinesi

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n1;
+        int n2;
+        int secim;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Birinci Sayıyı Giriniz: ");
+        n1 = input.nextInt();
+        System.out.print("İkinci Sayıyı Giriniz: ");
+        n2 = input.nextInt();
+
+        System.out.println("1-Toplama \n 2-Çıkarma \n 3-Çarpma \n 4- Bölme");
+        System.out.print("Seçiminizi Yapınız: ");
+        secim = input.nextInt();
+
+        switch (secim) {
+            case 1:
+                System.out.println("Toplam: " + (n1 + n2));
+                break;
+
+            case 2:
+                System.out.println("Çıkarma: " + (n1-n2));
+                break;
+
+            case 3:
+                System.out.println("Çarpma: "+ (n1*n2));
+                break;
+
+            case 4:
+                if ( n2 == 0) {
+                    System.out.println("Sayı sıfıra bölünemez.");
+                } else
+                    System.out.println("Bölme: " + (n1 / n2));
+                break;
+            default:
+                System.out.println("Tekrar deneyin");
+
+        }
+    }
+}
